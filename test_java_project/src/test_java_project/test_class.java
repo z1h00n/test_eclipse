dce47c6fb9cc3687575059a56e7a27543571e066
Do Ji-Hoon <zihoon9@gmail.com>
2019-03-27 10:39:34 +0900

    added print line

--- test_java_project/src/test_java_project/test_class.java
+++ test_java_project/src/test_java_project/test_class.java
@@ -3,6 +3,7 @@ package test_java_project;
 public class test_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("hello world~! hotfix!");
 		
 	}
