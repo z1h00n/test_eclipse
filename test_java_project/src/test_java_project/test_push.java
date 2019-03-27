2016d6c5b0c8ebd408a783f8965611873b3425ab
zihoon9 <48740154+zihoon9@users.noreply.github.com>
2019-03-27 11:10:16 +0900

    Update test_push.java

--- test_java_project/src/test_java_project/test_push.java
+++ test_java_project/src/test_java_project/test_push.java
@@ -2,6 +2,8 @@ package test_java_project;
 
 public class test_push {
   
-  System.out.println("modify at git hub(server)");
-
+  void func() {
+      System.out.println("modify at git hub(server)");
+      System.out.println("i'm local~!");
+      System.out.println("pull test");
 }
